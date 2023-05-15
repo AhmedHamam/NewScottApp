1d362aa8da1b6b5b9dc46a738e8accda3c89a598
AhmedHamam <ahmed.hamam50@gmail.com>
2023-05-15 18:43:42 +0300

    logrepository

---
+++ Base.Infrastructure/Logs/LogRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Transactions;
+
+namespace Base.Infrastructure.Logs
+{
+    public class LogRepository
+    {
+        LogDBContext _logDBContext = null;
+
+        public LogRepository()
+        {
+
+            _logDBContext = new LogDBContext();
+
+        }
+        internal void AddToLogs(BaseLogEntry BaseLogEntry)
+        {
+            try
+            {
+                if (BaseLogEntry != null)
+                {
+                    using (TransactionScope scope = new TransactionScope(TransactionScopeOption.RequiresNew))
+                    {
+                        _logDBContext.Add(BaseLogEntry);
+                        _logDBContext.SaveChanges();
+                        scope.Complete();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                RepositoryHelper.LogException(ex);
+            }
+        }
+
+    }
+}
